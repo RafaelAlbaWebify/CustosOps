dc11ee9ff163879c9d7b2993016e14239d47baa5
Rafael Alba <ralbas.int@gmail.com>
2026-07-16 08:20:07 +0200

    Apply Overview repair after dashboard theme styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
-import "./overview-responsive.css";
 import "./guiRuntimeFixes";
 import "./mock-dashboard-refresh.css";
+import "./overview-responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
